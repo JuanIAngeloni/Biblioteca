b92c9b974c543def47379e0a035c80ad34d350db
Juan Ignacio Angeloni 112069 <juaniangeloni@gmail.com>
2023-09-18 12:58:13 -0300

    Proyecto biblioteca
    proyecto biblioteca sin haber sido deployado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { BookCardComponent } from './book-card/book-card.component';
+import { BookImagenComponent } from './book-imagen/book-imagen.component';
+import { OpacityComponent } from './opacity/opacity.component';
+import { LoadingComponent } from './loading/loading.component';
+import { TitulosComponent } from './titulos/titulos.component';
+import { AntiguedadPorAntiguedadPipe } from './antiguedad-por-antiguedad.pipe';
+import { CargaDeLibrosComponent } from './carga-de-libros/carga-de-libros.component';
+import { BookService } from './book.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    BookCardComponent,
+    BookImagenComponent,
+    OpacityComponent,
+    LoadingComponent,
+    TitulosComponent,
+    AntiguedadPorAntiguedadPipe,
+    CargaDeLibrosComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+}
